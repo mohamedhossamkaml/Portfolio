d2c131365047f20f4719d41d1d3e4d8efcbf1927
Mohamed Hossam <https://github.com/mohamedhossamkaml>
2026-02-24 00:58:13 +0200

    add link

--- .history/vite.config_20260224005246.ts
+++ .history/vite.config_20260224005246.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/dar-el-meamar-landing/",
+  base: "/Portfolio/",
 
   plugins: [react()],
 
